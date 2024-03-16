3eea1467857b65c27ba4d1710a544d9ec684bff3
rimasultana <rimasultana20225@gmail.com>
2024-03-16 09:23:03 +0600

    itarable

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const array = [1, 2, 3, 4, 5];
+function myNumbers() {
+    let n = 0;
+    return {
+      next: function() {
+        n += 10;
+        return {value:n, done:false};
+      }
+    };
+  }
 
-for(i=0; i < array.length; i++) {
-    console.log(array[i]);
-}
+  const n = myNumbers();
+n.next(); // Returns 10
+n.next(); // Returns 20
+n.next(); // Returns 30
+
+console.log(n.next().value);
